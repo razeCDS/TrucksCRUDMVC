6fa90e1f076fb81d4d95d837df7d239b783fa832
Cezar Camargo da Silva <fe652@t-systems.com.br>
2023-04-03 17:23:44 -0300

    Truck Model and Controller Created

---
+++ Controllers/TrucksController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TrucksCRUD.Data;
+using TrucksCRUD.Models;
+
+namespace TrucksCRUD.Controllers
+{
+    public class TrucksController : Controller
+    {
+        private readonly TrucksCRUDContext _context;
+
+        public TrucksController(TrucksCRUDContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trucks
+        public async Task<IActionResult> Index()
+        {
+              return _context.Truck != null ? 
+                          View(await _context.Truck.ToListAsync()) :
+                          Problem("Entity set 'TrucksCRUDContext.Truck'  is null.");
+        }
+
+        // GET: Trucks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Truck == null)
+            {
+                return NotFound();
+            }
+
+            var truck = await _context.Truck
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (truck == null)
+            {
+                return NotFound();
+            }
+
+            return View(truck);
+        }
+
+        // GET: Trucks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trucks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,CreationDate")] Truck truck)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(truck);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(truck);
+        }
+
+        // GET: Trucks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Truck == null)
+            {
+                return NotFound();
+            }
+
+            var truck = await _context.Truck.FindAsync(id);
+            if (truck == null)
+            {
+                return NotFound();
+            }
+            return View(truck);
+        }
+
+        // POST: Trucks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,CreationDate")] Truck truck)
+        {
+            if (id != truck.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(truck);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TruckExists(truck.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(truck);
+        }
+
+        // GET: Trucks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Truck == null)
+            {
+                return NotFound();
+            }
+
+            var truck = await _context.Truck
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (truck == null)
+            {
+                return NotFound();
+            }
+
+            return View(truck);
+        }
+
+        // POST: Trucks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Truck == null)
+            {
+                return Problem("Entity set 'TrucksCRUDContext.Truck'  is null.");
+            }
+            var truck = await _context.Truck.FindAsync(id);
+            if (truck != null)
+            {
+                _context.Truck.Remove(truck);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TruckExists(int id)
+        {
+          return (_context.Truck?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/TrucksCRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrucksCRUD.Models;
+
+namespace TrucksCRUD.Data
+{
+    public class TrucksCRUDContext : DbContext
+    {
+        public TrucksCRUDContext (DbContextOptions<TrucksCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TrucksCRUD.Models.Truck> Truck { get; set; } = default!;
+    }
+}
